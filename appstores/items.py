ec9b2800ea303fcb91f716669af1b741e71d36e0
Pengfei Xue <pengphy@gmail.com>
2014-12-09 14:10:54 +0800

    add comment

--- appstores/items.py
+++ appstores/items.py
@@ -9,7 +9,28 @@ import scrapy
 
 
 class AppstoresItem(scrapy.Item):
-    # define the fields for your item here like:
+    '''
+        item that will be stored in db, the mean of fields are:
+        display_name:
+            name displayed in pages, eg. '欧朋浏览器'
+
+        package_name:
+            apk's package name, eg. 'com.oupeng.browser'
+
+        keyword:
+            keywords used to search in market, it can be seen in
+            `appstores.settings.SEARCH_KEYWORDS`
+
+        dlcount: download count
+        comemnt_count: comment count
+        category: apk's category in targeted market
+        update_time: apk's latest update time
+        version: apk's version
+        ranking: the order in search result
+        rating: rating in targeted market
+        store_name: targeted app store's name
+    '''
+
     display_name = scrapy.Field()
     package_name = scrapy.Field()
     keyword = scrapy.Field()
